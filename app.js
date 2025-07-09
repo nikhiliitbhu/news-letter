47045d34c7816c861c472fb03a091116b7d6293d
NIKHIL SINGH <96346997+nikhiliitbhu@users.noreply.github.com>
2025-07-09 11:49:42 +0530

    added all files

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const https = require('https');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`server up at ${process.env.PORT || 3000}}`);
+})
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/signup.html")
+})
+
+app.post("/signup", function(req, res){
+    const {firstName, lastName, email} = req.body;
+
+    let data = {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+    };
+
+    let jsonData = JSON.stringify(data);
+    
+    const serverPrefix = 'us9';
+    const listID = 'd9c627ce06';
+    const apiKey = "663a3a6b4e0322af6a33ddb1b37db47a";
+    const url = `https://${serverPrefix}.api.mailchimp.com/3.0/lists/${listID}/members`
+    
+    const options = {
+        method: "post",
+        auth: `nikhil:${apiKey}`
+    }
+    
+    const call = https.request(url, options, function(response){
+
+        if(response.statusCode == 200){
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+
+        response.on('data', function(data){
+            console.log(JSON.parse(data));
+        })
+
+        response.on('error', function(e){
+            console.log(e);
+        })
+    })
+
+
+    call.write(jsonData);
+    call.end();
+    // res.send(JSON.stringify(req.body));
+})
+
+
+//MailChimp
+//API key: 663a3a6b4e0322af6a33ddb1b37db47a-us9
+//List ID: d9c627ce06
